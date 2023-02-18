0d27c1af5dd868a66d3a4d149530f8dac5385439
Karan <karanchandravanshi217@gmail.com>
2023-02-18 14:08:22 +0530

    Completed

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
+import { Link } from "react-scroll";
 import { themeContext } from "../../Context";
 import { useContext } from "react";
-const Card = ({photo, heading,detail}) => {
+const Card = ({ photo, heading, detail }) => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div className='card'>
-      <img src={photo}/>
-      <span style={{color:darkMode?'white':''}} >{heading}</span>
+    <div className="card">
+      <img src={photo} />
+      <span style={{ color: darkMode ? "white" : "" }}>{heading}</span>
       <span>{detail}</span>
-      <button className='c-button'style={{color:darkMode?'white':''}}>Check Out My Recent Projects</button>
+      <Link spy={true} to="Project" smooth={true} activeClass="activeClass">
+        {" "}
+        <button className="c-button" style={{ color: darkMode ? "white" : "" }}>
+          Check Out My Recent Projects
+        </button>
+      </Link>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
